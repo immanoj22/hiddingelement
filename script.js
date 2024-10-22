7cd531d8abc8b139e7731b317def4fcef19f6ffb
Manoj <132736374+immanoj22@users.noreply.github.com>
2024-10-22 22:02:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let box=document.getElementById("box1");
+let btn=document.getElementById("btn");
+box.style.display="none";
+btn.addEventListener("click", event=>{
+    if(box.style.display==="none"){
+        box.style.display="block"
+        btn.textContent="Hide❤️"
+        box.style.display="flex";
+        box.style.justifyContent="center";
+        box.style.alignItems="center";
+       
+    }
+    else{
+        box.style.display="none"
+        btn.textContent="Surprise😶‍🌫️"
+        
+        
+    }
+})
